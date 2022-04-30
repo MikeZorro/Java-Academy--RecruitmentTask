de4d794f833ef1866af3894628155bfd5db126b5
Michal Soroczynski <soroczynski.m@gmail.com>
2022-04-30 15:05:46 +0200

    added the functionality to display only unique letters from the pasted argument

--- MyApp/src/Main/Main.java
+++ MyApp/src/Main/Main.java
@@ -5,30 +5,43 @@ import org.apache.commons.lang3.StringUtils;
 
 
 public class Main {
-    public static String toLowerCase(String str){
+    public static String toLowerCaseAndRemoval(String str){
         String str2 = StringUtils.deleteWhitespace(str.toLowerCase(Locale.ROOT));
-
         return str2;
     }
 
+    public static long uniqueCharactersCounter (String str) {
+        return str.chars().distinct().count();
+    }
+
+
     public static void letterCounter (String str) {
-        String str2 = toLowerCase(str);
+        String str2 = toLowerCaseAndRemoval(str);
         char[] charArray = str2.toCharArray();
         int counter = 0;
+        StringBuilder reading = new StringBuilder();
         for (int i = 0; i < charArray.length; i++) {
             for (int j = 0; j < charArray.length; j++) {
                 if (charArray[i] == charArray[j]) {
                     counter++;
                 }
             }
-            String result = ("" + charArray[i] + counter);
-            System.out.println(result);
+            String result = ("" + charArray[i] + counter +" ");
+            reading.append(result);
             counter = 0;
         }
+            String finalResult = new String (String.valueOf(reading));
+        String [] result = finalResult.split(" ");
+        String [] uniqueResults = Arrays.stream(result).distinct().toArray(String[]:: new);
+        for(String s : uniqueResults) {
+            System.out.println(s);
+        }
+
+
     }
 
 
             public static void main (String[]args){
-                letterCounter("MyApp JavaAcademy");
+       letterCounter("MyApp JavaAcademy");
             }
         }
