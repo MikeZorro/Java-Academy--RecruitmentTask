b8ff0c9e9a59066985de932a1b74957c7d28e8bb
Michal Soroczynski <soroczynski.m@gmail.com>
2022-04-30 13:49:24 +0200

    Imported apache.commons to build helper method to delete white spaces and lower case the String
    Created first draft of the letterCounter method that converts String to char [] and prints them one by one

--- MyApp/src/Main/Main.java
+++ MyApp/src/Main/Main.java
@@ -1,10 +1,25 @@
 package Main;
+import java.util.Arrays;
+import java.util.Locale;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Main {
-    public static String letterCounter (String str){
-        return str;
+    public static String toLowerCase(String str){
+        String str2 = StringUtils.deleteWhitespace(str.toLowerCase(Locale.ROOT));
+
+        return str2;
     }
 
-    public static void main(String[] args) {
-    }
-}
+    public static void letterCounter (String str) {
+        String str2 = toLowerCase(str);
+        char[] charArray = str2.toCharArray();
+        int counter = 0;
+
+        }
+
+
+            public static void main (String[]args){
+                letterCounter("MyApp JavaAcademy");
+            }
+        }
